4af6ff78d84541d046b279dbda23d716eb70fff9
Nikita Korchemnyi <FrontDevKN@proton.me>
2024-07-15 16:12:17 +0200

    CLI_Course01_02

---
+++ 01_CLI_Interactive_Sort/index.js
@@ -0,0 +1,111 @@
+const { stdin: input, stdout: output } = require('process')
+const readLine = require('readline')
+
+let rl = readLine.createInterface({
+	input, output
+})
+
+let sortedMassive = []
+let step = 0;
+let accesValue = 10;
+
+const variantSort = {
+	a: "Enter 'a': Sort the words alphabetically.\n",
+	b: "Enter 'b': Display the numbers in ascending order.\n",
+	c: "Enter 'c': Display the numbers in descending order.\n",
+	d: "Enter 'd': Display the words in ascending order based on the number of letters in each word.\n",
+	e: "Enter 'e': Show only unique words.\n",
+	f: "Enter 'f': Show only the unique values from the entire set of words and numbers entered by the user.\n",
+	exit: "Enter 'Exit': For exit programm. \n"
+};
+
+const variantFunc = () => {
+	console.log(variantSort.a, variantSort.b, variantSort.c, variantSort.d, variantSort.e, variantSort.f, variantSort.exit);
+}
+const accesValuefn = () => {
+	console.log(`Введите ${accesValue} значений через пробел`);
+}
+
+
+console.log(`Введите ${accesValue} значений через пробел`);
+rl.on('line', (line) => {
+
+	switch (step) {
+
+		case 0:
+			let moment = line.trim().split(' ')
+
+			if (moment.length !== accesValue) {
+				console.log(`Нужно ввести именно ${accesValue} значений:(`);
+			} else {
+				moment.forEach((value) => {
+					if (isNaN(value)) {
+						sortedMassive.push(value)
+					} else {
+						sortedMassive.push(parseFloat(value))
+					}
+				})
+				console.log(sortedMassive);
+				step++
+				variantFunc()
+			}
+			break;
+
+		case 1:
+			switch (line) {
+				case 'a': {
+					// logick Sort the words alphabetically.
+					let variantA = sortedMassive.filter((item) => typeof item === 'string').sort()
+					console.log(`Ты выбрал вариант ${variantSort.a} \n Вот твой массив : ${variantA} \n`);
+					accesValuefn()
+					step--
+					break;
+				}
+				case 'b': {
+					// logick Display the numbers in ascending order.
+					let variantB = sortedMassive.filter((item) => typeof item === 'number').sort((a, b) => a - b)
+					console.log(`Ты выбрал вариант ${variantSort.b} \n Вот твой массив : ${variantB} \n`)
+					accesValuefn()
+					step--
+					break;
+				}
+				case 'c': {
+					// logick Display the numbers in descending order.
+					let variantC = sortedMassive.filter((item) => typeof item === 'number').sort((a, b) => b - a)
+					console.log(`Ты выбрал вариант ${variantSort.c} \n Вот твой массив : ${variantC} \n`)
+					accesValuefn()
+					step--
+					break;
+				}
+				case 'd': {
+					// logick Display the words in ascending order based on the number of letters in each word.
+					let variantD = sortedMassive.filter((item) => typeof item === 'string').sort((a, b) => a.length - b.length)
+					console.log(`Ты выбрал вариант ${variantSort.d} \n Вот твой массив : ${variantD} \n`)
+					accesValuefn()
+					step--
+					break;
+				}
+				case 'e': {
+					// logick Show only unique words.
+					let variantE = [...new Set(sortedMassive.filter((item) => typeof item === 'string'))]
+					console.log(`Ты выбрал вариант ${variantSort.e} \n Вот твой массив : ${variantE} \n`)
+					accesValuefn()
+					step--
+					break;
+				}
+				case 'f': {
+					// logick Show only the unique values from the entire set of words and numbers entered by the user.
+					let variantF = [...new Set(sortedMassive)]
+					console.log(`Ты выбрал вариант ${variantSort.f} \n Вот твой массив : ${variantF} \n`)
+					accesValuefn()
+					step--
+					break;
+				}
+				case 'Exit': {
+					rl.close()
+					break;
+				}
+			}
+			break;
+	}
+})
---
+++ 02_CLI_Pimitive_DB/index.js
@@ -0,0 +1,53 @@
+import { input, number, select, confirm } from '@inquirer/prompts'
+
+const db = [
+	{
+		name: 'Nikita', gender: 'Man', age: 18
+	},
+	{
+		name: 'Masha', gender: 'Woman', age: 17
+	},
+	{
+		name: 'Guse', gender: 'Man', age: 15
+	}]
+
+const createUser = async () => {
+	const name = await input({ message: 'Enter your name' });
+	const gender = await select({
+		message: 'Gender',
+		choices: [
+			{
+				name: 'Man',
+				value: 'Man',
+				description: 'Your gender a Man !',
+			},
+			{
+				name: 'Woman',
+				value: 'Woman',
+				description: 'Your gender a Woman !',
+			},
+		]
+	})
+	const age = await number({ message: 'Enter your age' });
+	db.push({ name, gender, age });
+	console.table(db);
+
+	const addAnother = await confirm({ message: 'Do you want to add another user?' })
+	if (addAnother) {
+		await createUser()
+	} else {
+		const searchUser = await confirm({ message: 'Хочешь найти пользователя по Нику ?' })
+		if (searchUser) {
+			const searchUserName = await input({ message: 'Enter the name of the user you want to find' });
+			const user = db.find((app) => app.name === searchUserName);
+			if (user) {
+				console.log(`${searchUserName} is found:`, user);
+			} else {
+				console.log(`${searchUserName} is not found.`);
+			}
+		}
+	}
+}
+
+createUser()
+
