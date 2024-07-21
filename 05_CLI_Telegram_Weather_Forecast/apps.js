02f3334cfb074dcf3fa5ad5145aba4f7d0f31228
Nikita Korchemnyi <worflersst@proton.me>
2024-07-21 13:01:45 +0200

    cli_tusk05

---
+++ 05_CLI_Telegram_Weather_Forecast/apps.js
@@ -0,0 +1,40 @@
+const TelegramBot = require('node-telegram-bot-api')
+const { getWeatherData, city } = require('./weather');
+
+
+const token = '7456352495:AAF68oAaAJS_M_vPhkNegJt8GVXZULdMWEE'
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, async (msg) => {
+	const chatId = msg.chat.id;
+	await bot.sendMessage(chatId, 'Выберите', {
+		reply_markup: {
+			keyboard: [
+				[`Weather forecast in ${city}`],
+				['3-hour interval', '6-hour interval']
+			],
+			resize_keyboard: true
+		}
+	});
+});
+
+bot.on('message', async (msg) => {
+	const chatId = msg.chat.id;
+	const text = msg.text;
+
+	if (text === `Weather forecast in ${city}`) {
+		await bot.sendMessage(chatId, `Here is the weather forecast for ${city}.`);
+	} else if (text === '3-hour interval') {
+		const weatherData3 = await getWeatherData(3);
+		await bot.sendMessage(chatId, weatherData3);
+	} else if (text === '6-hour interval') {
+		const weatherData6 = await getWeatherData(6);
+		await bot.sendMessage(chatId, weatherData6);
+	}
+});
+
+
+
+
+//getWeatherData(3)
+//getWeatherData(6)
---
+++ 05_CLI_Telegram_Weather_Forecast/weather.js
@@ -0,0 +1,65 @@
+
+
+const apiKey = '077e676529e73c2072be4463a0fea680';
+const city = 'Odessa'
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=ru&cnt=40`;
+
+const filterData3Hours = (data) => data.list;
+const filterData6Hours = (data) => data.list.filter((_, index) => index % 2 === 0);
+const formatWeatherData = (data, city) => {
+	const days = {};
+
+	data.forEach(forecast => {
+		const date = new Date(forecast.dt * 1000);
+		const day = date.toLocaleDateString('ru-RU', {
+			weekday: 'long',
+			day: 'numeric',
+			month: 'long'
+		});
+		const time = date.toLocaleTimeString('ru-RU', {
+			hour: '2-digit',
+			minute: '2-digit'
+		});
+
+		const temp = forecast.main.temp;
+		const feelsLike = forecast.main.feels_like;
+		const weatherDescription = forecast.weather[0].description;
+
+		if (!days[day]) {
+			days[day] = [];
+		}
+
+		days[day].push(`${time}, +${temp.toFixed(0)}°C, ощущается: +${feelsLike.toFixed(0)}°C, ${weatherDescription}`);
+	});
+
+	let result = `Погода в ${city}:\n\n`;
+	Object.keys(days).forEach(day => {
+		result += `${day}:\n`;
+		days[day].forEach(entry => {
+			result += `  ${entry}\n`;
+		});
+		result += '\n';
+	});
+
+	return result;
+};
+
+const getWeatherData = async (interval) => {
+
+	const response = await fetch(url);
+	const data = await response.json();
+	const city = data.city.name;
+
+	let filteredData;
+	if (interval === 3) {
+		filteredData = filterData3Hours(data);
+	} else if (interval === 6) {
+		filteredData = filterData6Hours(data);
+	}
+
+	const formattedData = formatWeatherData(filteredData, city);
+	return formattedData
+};
+
+module.exports = { getWeatherData, city };
+
