511d80ed7022577204046169599206ed487f102b
Nikita Korchemnyi <worflersst@proton.me>
2024-08-15 22:24:57 +0300

    add express for deploy tusk5

--- CLI_Course/05_CLI_Telegram_Weather_Forecast/apps.js
+++ CLI_Course/05_CLI_Telegram_Weather_Forecast/apps.js
@@ -1,6 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api')
 const { getWeatherData, city } = require('./weather');
 require('dotenv').config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Bot is alive");
+});
+
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
+
 
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
