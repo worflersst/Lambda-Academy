97c75bd1c023a1b12aa34f9c9ca9a18b523ea584
Nikita Korchemnyi <worflersst@proton.me>
2024-08-26 18:29:16 +0300

    add interface

--- Stack_Course/Frontend/01_Frontend_Authorization/src/models/response/AuthResponse.ts
+++ Stack_Course/Frontend/01_Frontend_Authorization/src/models/response/AuthResponse.ts
@@ -1,10 +1,18 @@
-export interface AuthResponse {
+export interface AuthResponsOrError {
   body: {
     access_token?: string;
     refresh_token?: string;
     code?: number;
     message?: string;
     status?: string;
+  };
+  statusCode: number;
+}
+
+export interface AuthResponse {
+  body: {
+    access_token: string;
+    refresh_token: string;
   };
   statusCode: number;
 }
