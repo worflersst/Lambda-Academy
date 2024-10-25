fb95d1086f4e827e0ace80d84ae720eb74db82f3
Korchemnyi Nikita <worflersst@proton.me>
2024-10-25 14:43:20 +0300

    add: useWindowSize hook

---
+++ Stack_Course/Frontend/02_Frontend_Landing/src/shared/hooks/useWindowSize/useWindowSize.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+export const useWindowSize = () => {
+	const [windowSize, setWindowSize] = useState<{
+		width: number;
+		height: number;
+	}>({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	});
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		handleResize();
+
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	return windowSize;
+};
